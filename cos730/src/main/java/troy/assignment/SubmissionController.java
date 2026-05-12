37b9efcb92856e19aef10545f1a2062626436674
Troy Clark <TClark@dvtsoftware.com>
2026-05-12 11:22:07 +0200

    feat: removed logic from SubmissionController

--- cos730/src/main/java/troy/assignment/SubmissionController.java
+++ cos730/src/main/java/troy/assignment/SubmissionController.java
@@ -3,7 +3,6 @@ package troy.assignment;
 import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.List;
 import java.util.function.Consumer;
 
 public class SubmissionController {
@@ -13,18 +12,9 @@ public class SubmissionController {
         this.onStatus = onStatus;
     }
 
-    public void submit(File selectedFile) throws IOException, SQLException {
+    public void submit(File data) throws IOException, SQLException {
         Database db = new Database();
-        int submissionId = db.saveSubmission(selectedFile);
-        ReviewerManager rm = new ReviewerManager(db);
-        List<Reviewer> filteredReviewers = rm.getAvailableReviewers(submissionId);
-        onStatus.accept("Valid - " + filteredReviewers.size() + " reviewers available");
-        NotificationService ns = new NotificationService(message -> javafx.application.Platform.runLater(() -> onStatus.accept(message)));
-        EvaluationManager em = new EvaluationManager(db, ns);
-        System.out.println("loop [assign reviewers]");
-        for (Reviewer reviewer : filteredReviewers) {
-            reviewer.assignReview(submissionId);
-        }
-        em.startEvaluation(submissionId, filteredReviewers);
+        int researchId = db.saveSubmission(data);
+        onStatus.accept("Submission saved with ID: " + researchId);
     }
 }
